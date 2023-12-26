02ef5eceefe7c4d2408aeb49194cc21b0ba3690c
Guney Unay <guneymarketing17@hotmail.com>
2023-12-26 18:01:29 +0300

    added success stories design for all devices screen

--- containers/HomeContainer/success-stories/index.jsx
+++ containers/HomeContainer/success-stories/index.jsx
@@ -6,18 +6,18 @@ function SuccessStories() {
   return (
     <section className="bg-[#004047] bg-hero-image-3 bg-left-top bg-no-repeat">
       <div className="w-full h-full bg-hero-image-4 bg-no-repeat bg-right-bottom">
-        <div className="container mx-auto flex flex-col gap-12 py-[120px] px-6 md:px-0 text-white">
+        <div className="container mx-auto flex flex-col lg:items-center gap-12 lg:gap-14 py-[120px] lg:py-[140px] px-6 md:px-0 text-white">
           <div>
-            <h3 className="text-white text-[32px] font-bold leading-8 text-center ">
+            <h3 className="text-white text-[32px] lg:text-[48px] font-bold leading-8 text-center lg:leading-[48px] lg:max-w-[30ch] ">
               Delivering real results for top componies. Some of out &nbsp;
-              <span className="text-[#79c8c7]">success stories.</span>
+              <span className="text-[#79c8c7] ">success stories.</span>
             </h3>
           </div>
-          <div className="flex flex-col gap-12">
+          <div className="flex flex-col lg:flex-row gap-12">
             {CARDS.map((card) => (
               <div
                 key={card.id}
-                className="flex flex-col items-center gap-4 text-center bg-quotes bg-top bg-no-repeat pt-9 "
+                className="flex flex-col items-center gap-4 text-center bg-quotes bg-top bg-no-repeat pt-9 lg:flex-1 "
               >
                 <p className="text-[15px] font-semibold leading-6 ">
                   {card.content}
