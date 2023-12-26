2bf19d7ac1a15d30159ac797efd3c1302a8e6701
Guney Unay <guneymarketing17@hotmail.com>
2023-12-26 16:18:13 +0300

    added succes section constant.js

---
+++ containers/HomeContainer/success-stories/constant.js
@@ -0,0 +1,27 @@
+import AvatarKady from "@/assets/images/avatar-kady.jpg";
+import AvatarReese from "@/assets/images/avatar-kady.jpg";
+import AvatarClarke from "@/assets/images/avatar-kady.jpg";
+
+export const CARDS = [
+  {
+    id: 0,
+    content: ` “The team perfectly fit the specialized skill set required. They focused on the most essential features helping us launch the platform eight months faster than planned.”`,
+    author: "Kady Baker",
+    job: "Product Manager at Bookmark",
+    image: AvatarKady,
+  },
+  {
+    id: 1,
+    content: `“We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!”`,
+    author: "Aiysha Reese",
+    job: "Founder of Manage",
+    image: AvatarReese,
+  },
+  {
+    id: 2,
+    content: `“Amazing. Our team helped us build an app that delivered a new experience for hiring a physio. The launch was an instant success with 100k downloads in the first month.”`,
+    author: "Arthur Clarke",
+    job: "Co-founder of MyPhysio",
+    image: AvatarClarke,
+  },
+];
