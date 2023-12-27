acf0cf727aa7ef1bdcaa7bc2da0534b5b3e9ddbd
Guney Unay <guneymarketing17@hotmail.com>
2023-12-27 21:40:45 +0300

    added transition to card

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import ButtonPlus from "./button-image-1.svg";
 import ButtonClose from "./button-image-2.svg";
+import SocialLink from "./social-links.svg";
+import clsx from "clsx";
 
 function Card({ card }) {
+  const [isActive, setActive] = useState(false);
   return (
     <div className="w-full md:w-[49%] lg:w-[32%]   bg-[#012f34] flex flex-col items-center py-8  gap-4 relative md:mb-[76px]">
       <div className="relative w-24 h-24 border-2 border-[#79C8C7] rounded-full">
@@ -25,9 +29,35 @@ function Card({ card }) {
         </h4>
       </div>
 
-      <button className="absolute bottom-0 left-1/2 inline-flex items-center justify-center -translate-x-1/2  translate-y-1/2 cursor-pointerw">
-        <ButtonPlus />
+      <button
+        onClick={() => setActive(!isActive)}
+        className="absolute bottom-0 left-1/2 inline-flex items-center justify-center -translate-x-1/2  translate-y-1/2 cursor-pointer z-10"
+      >
+        {isActive ? <ButtonClose /> : <ButtonPlus />}
       </button>
+
+      {/* Active card */}
+
+      <div
+        className={clsx(
+          "absolute inset-0 bg-[#002529] transition-opacity delay-150",
+          { "opacity-100": isActive },
+          { "opacity-0": !isActive }
+        )}
+      >
+        <div className="flex flex-col items-center py-8 gap-2 px-7 ">
+          <h3 className="text-[#79c8c7] text-lg font-bold leading-7">
+            {card.name}
+          </h3>
+          <p className="text-white text-[15px] font-semibold leading-6 ">
+            {card.content}
+          </p>
+
+          <div className="mt-4 cursor-pointer">
+            <SocialLink />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- containers/AboutContainer/meet-section/index.jsx
+++ containers/AboutContainer/meet-section/index.jsx
@@ -6,7 +6,7 @@ function MeetSection() {
     <section className="bg-[#004047] bg-cards-1 bg-no-repeat bg-left-top">
       <div className="w-full h-full bg-cards-2 bg-no-repeat bg-right-bottom ">
         <div className="container mx-auto px-6 md:px-0">
-          <div className="flex flex-col items-center pt-[88px] pb-[116px] gap-12 md:gap-16 text-white">
+          <div className="flex flex-col items-center pt-[88px] pb-[116px] gap-12 md:gap-16 text-white text-center">
             <h3 className="text-[32px] lg:text-[48px]  font-bold leading-8">
               Meet the directors
             </h3>
